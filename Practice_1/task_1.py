af38763c1c9e2001c58bb98e17d06bc1b478a056
benican <nic79@list.ru>
2018-10-16 23:10:03 +1000

    Initial commit.

--- Practice_1/task_1.py
+++ Practice_1/task_1.py
@@ -2,10 +2,18 @@
 Длина Московской кольцевой автомобильной дороги - 109 километров. Стартуем с нулевого километра МКАД и едем со скоростью V километров в час. На какой отметке остановимся через T часов? Программа получает на вход значение V и T. Если V>0, то движемся в положительном направлении по МКАД, если же значение V<0, то в отрицательном. Программа должна вывести целое число от 0 до 108 - номер отметки, на которой остановимся
 """
 
-v = int(input())
-t = int(input())
+speed = int(input('Введите скорость движения, км/ч. '))
+time = int(input('Введите время движения, ч. '))
+circle_length = 109
 
-print((v * t) % 109)
+if speed > 0:    
+    number_of_circle = (speed*time)/circle_length
+    stop_point = round(circle_length * (number_of_circle - int(number_of_circle)))
+else:
+    number_of_circle = (speed*time)/circle_length
+    stop_point = circle_length - abs(round(circle_length * (number_of_circle - int(number_of_circle))))
+
+print("Вы остановились на" + " " + str(stop_point) + " " + "км.")
 
 
 
--- Practice_1/task_2.py
+++ Practice_1/task_2.py
@@ -2,10 +2,15 @@
 Процентная ставка по вкладу состовляет P процентов годовых, которые прибавляются к сумме вклада. Вклад состовляет X рублей Y копеек.Определите размер вклада через год. Программа получает на вход целые числа P,X,Y и должна вывести два числа: величину вклада через год в рублях и копейках. Дробная часть копее отбрасывается.
 """
 
-p = int(input())
-x = int(input())
-y = int(input())
-deposite_before = 100 * x + y
-deposite_after = int(deposite_before * (1 + p/100))
-print(deposite_after // 100, deposite_after % 100)
+interest_rate = int(input('Введите годовую ставку процента, %. '))
+bank_deposite = input('Введите размер банковского вклада, руб.коп ').split(".")
+
+rub = int(bank_deposite[0])
+cent = int(bank_deposite[1])
+
+bank_deposite_total = ((rub*100+cent)*(1+interest_rate/100))/100
+rub = int(bank_deposite_total)
+cent = round(100*(bank_deposite_total-int(bank_deposite_total)))
+
+print('Ваш банковский депозит равен' + ' ' + str(rub) + ' ' + 'рублей' + ' ' +str(cent) + ' '+ 'копеек')
 
